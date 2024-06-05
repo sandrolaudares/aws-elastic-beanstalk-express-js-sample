940a2d8fa02debc2861a68e03397e61bb79be39c
Sandro Laudares <110068@sga.pucminas.br>
2024-06-05 10:26:49 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('TESTE DE PIPELINE'));
+app.get('/', (req, res) => res.send('ULTIMO TESTE PIPELINE'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
